25ddae2a570db58e67ac21a7957fd4537f50c79a
VeeraMahendra2003 <97424987+VeeraMahendra2003@users.noreply.github.com>
2022-03-08 21:22:34 +0530

    #include<stdio.h> int main() {     int n,i,c=0,d,j=0,s=0,k=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         while(n)         {             s=0;             d=n%10;             j++;             for(i=1;i<=d;i++)             {                 if(d%i==0)                 {                     s++;                 }             }             if(s==2)             {                 k++;             }             n=n/10;         }         if(k==j)         {             printf("Mega Prime");             return 0;         }         printf("Not Mega Prime");     }     else     {         printf("Not Mega Prime");     } }

---
+++ mega_prime.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,d,j=0,s=0,k=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        while(n)
+        {
+            s=0;
+            d=n%10;
+            j++;
+            for(i=1;i<=d;i++)
+            {
+                if(d%i==0)
+                {
+                    s++;
+                }
+            }
+            if(s==2)
+            {
+                k++;
+            }
+            n=n/10;
+        }
+        if(k==j)
+        {
+            printf("Mega Prime");
+            return 0;
+        }
+        printf("Not Mega Prime");
+    }
+    else
+    {
+        printf("Not Mega Prime");
+    }
+}
